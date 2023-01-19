2c262f13c28f44ea9910ed0ce8d785fb6dd00640
Jahongir2109 <jahongirachilov004@gmail.com>
2023-01-19 15:11:39 +0500

    piramid changed

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -3,16 +3,22 @@ package school.mjc.stage0.loops.finalTask;
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
         for (int i = 1; i <= cathetusLength; i++) {
-            for (int j = 1; j <= cathetusLength - i; j++) {
-                System.out.print(" ");
+            String str="";
+            for (int j = 1; j <= cathetusLength; j++) {
+                if (i>=j){
+                    str=j+str;
+                }else {
+                    str=" "+str;
+                }
             }
-            for (int j = i; j > 0; j--) {
-                System.out.print(j);
+            for (int k = 2; k <= cathetusLength; k++) {
+                if (i>=k){
+                    str=str+k;
+                }else {
+                    str=str+" ";
+                }
             }
-            for (int j = 2; j <= i; j++) {
-                System.out.print(j);
-            }
-            System.out.println();
+            System.out.println(str);
         }
     }
 
